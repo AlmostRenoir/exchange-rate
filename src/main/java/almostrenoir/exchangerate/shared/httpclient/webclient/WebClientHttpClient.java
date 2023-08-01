11852a7c240ad337ccfdc34889a28f6933749fd6
Rafał Augustyn <r.augustyn96@gmail.com>
2023-08-01 21:09:41 +0200

    add http client

---
+++ src/main/java/almostrenoir/exchangerate/shared/httpclient/webclient/WebClientHttpClient.java
@@ -0,0 +1,36 @@
+package almostrenoir.exchangerate.shared.httpclient.webclient;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.stereotype.Component;
+import org.springframework.web.reactive.function.client.WebClient;
+import reactor.core.publisher.Mono;
+import java.time.Duration;
+import java.util.concurrent.TimeoutException;
+
+import almostrenoir.exchangerate.shared.httpclient.HttpClient;
+import almostrenoir.exchangerate.shared.httpclient.HttpException;
+
+@Component
+public class WebClientHttpClient implements HttpClient {
+
+    private final WebClient webClient;
+
+    public WebClientHttpClient() {
+        this.webClient = WebClient.create();
+    }
+
+    @Override
+    public <T> Mono<T> get(String url, Class<T> responseType, int timeout) {
+        return webClient.get()
+                .uri(url)
+                .retrieve()
+                .onStatus(HttpStatus.NOT_FOUND::equals,
+                        response -> Mono.error(new HttpException.NotFound())
+                ).onStatus(HttpStatusCode::is5xxServerError,
+                        response -> Mono.error(new HttpException.ServerError())
+                ).bodyToMono(responseType)
+                .timeout(Duration.ofMillis(timeout))
+                .onErrorMap(TimeoutException.class, ex -> new HttpException.TimeoutExceeded());
+    }
+}
